69d5af453f49e5d99f5e5869526b86324c02278f
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-16 10:26:52 -0300

    Criado Arquivo de configuracao

---
+++ app/config.js
@@ -0,0 +1,8 @@
+const Config = {
+	metaoee: true, // Marque como false caso trabalhado com PCT de Meta 
+	valores: { // Valores para alertas
+		vermelho: 0.0, // Valores acima de
+		amarelo: 0.0, // Valores acimda de e abaixo do anterior
+		verde: 0.0, // Valores abaixo de
+	},
+}
