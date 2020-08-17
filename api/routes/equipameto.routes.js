c32974eba013b32a5d2f5caf0cff08193a135909
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-17 10:31:37 -0300

    Removido JSON para teste

--- api/routes/equipameto.routes.js
+++ api/routes/equipameto.routes.js
@@ -8,48 +8,4 @@ module.exports = (app) => {
 	// Retrieve a single Equipament with equipamentoId
 	app.get("/equipamentos/:equipamentoId", equipamento.findOne);
 
-	app.get("/eqp", (req, res) => {
-		res.send(JSON.stringify([
-			{
-				"id_equipamento": 1,
-				"equipamento": "Nome Equipamento 1",
-				"unidade": "PC",
-				"horaatual": "2020-08-15T18:24:46.000Z",
-				"metamin": 5.683333333,
-				"producao": 892.34174,
-				"retrabalho": 0,
-				"metaatual": 2557.49999985,
-				"disponibilidade": 87.82222222,
-				"performance": 24.984229565,
-				"qualidade": 100,
-				"pctmeta": 34.891172632,
-				"ritmodia": 1.982981644,
-				"ritmometa": 9.0375,
-				"ritmomin": 12.12,
-				"inicioparada": "2020-08-14T18:24:46.000Z",
-				"TempoParado": "242:07:08",
-				"metaoee": 37.984229565
-			},
-			{
-				"id_equipamento": 2,
-				"equipamento": "Nome Equipamento 2",
-				"unidade": "M2UV",
-				"horaatual": "2020-08-15T18:24:46.000Z",
-				"metamin": 5.683333333,
-				"producao": 892.34174,
-				"retrabalho": 0,
-				"metaatual": 2557.49999985,
-				"disponibilidade": 87.82222222,
-				"performance": 24.984229565,
-				"qualidade": 100,
-				"pctmeta": 34.891172632,
-				"ritmodia": 1.982981644,
-				"ritmometa": 9.0375,
-				"ritmomin": 13.12,
-				"inicioparada": null,
-				"TempoParado": "242:07:08",
-				"metaoee": 37.984229565
-			}
-		]));
-	});
 }
