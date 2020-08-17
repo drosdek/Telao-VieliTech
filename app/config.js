63244801a881e7696560e27c87bc926d83f9a535
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-17 16:00:10 -0300

    Adicionado novas opcoes de configuracao

--- app/config.js
+++ app/config.js
@@ -1,8 +1,33 @@
 const Config = {
-	metaoee: true, // Marque como false caso trabalhado com PCT de Meta 
+	metaoee: true, // Marque como false caso trabalhado com PCT de Meta
+	pctmeta: false,
 	valores: { // Valores para alertas
-		vermelho: 0.0, // Valores acima de
-		amarelo: 0.0, // Valores acimda de e abaixo do anterior
-		verde: 0.0, // Valores abaixo de
+		vermelho: 65.0, // Valores acima de
+		amarelo: 76.0, // Valores acimda de e abaixo do anterior
+		verde: 100.0, // Valores abaixo de
 	},
+	pctdisponibilidade: true,
+	disponibilidade: {
+		vermelho: 10.0, // Valores acima de
+		amarelo: 20.0, // Valores acimda de e abaixo do anterior
+		verde: 100.0, // Valores abaixo de
+	},
+	pctperformance: true,
+	performance: {
+		vermelho: 50.0, // Valores acima de
+		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		verde: 100.0, // Valores abaixo de
+	},
+	pctqualidade: true,
+	qualidade: {
+		vermelho: 50.0, // Valores acima de
+		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		verde: 100.0, // Valores abaixo de
+	},
+	pctoee: true,
+	oee: {
+		vermelho: 50.0, // Valores acima de
+		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		verde: 100.0, // Valores abaixo de
+	}
 }
