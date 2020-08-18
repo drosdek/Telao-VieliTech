80f695f98ef75eb887b167f8ad0d5813b1de6e7b
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-18 10:56:55 -0300

    Adicionado novos parametros de Configuracao

--- app/config.js
+++ app/config.js
@@ -6,6 +6,12 @@ const Config = {
 		amarelo: 76.0, // Valores acimda de e abaixo do anterior
 		verde: 100.0, // Valores abaixo de
 	},
+	pctretrabalho: true,
+	retrabalho: {
+		vermelho: 100.0, // Valores acima de
+		amarelo: 5.0, // Valores acimda de e abaixo do anterior
+		verde: 0.0, // Valores abaixo de
+	},
 	pctdisponibilidade: true,
 	disponibilidade: {
 		vermelho: 10.0, // Valores acima de
@@ -29,5 +35,7 @@ const Config = {
 		vermelho: 50.0, // Valores acima de
 		amarelo: 55.0, // Valores acimda de e abaixo do anterior
 		verde: 100.0, // Valores abaixo de
-	}
+	},
+	//Cada segundo = 1000
+	tempodetransicao: 15000,
 }
