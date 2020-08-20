96c26784cf013792f7baf14f642b6b452db4b8af
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-20 13:16:39 -0300

    Definido condicoes e filtro tabela

--- app/pages/js/relatorio.js
+++ app/pages/js/relatorio.js
@@ -1,17 +1,71 @@
+
 const alimentaRel = (relatorio) => {
+	
 	for (t in relatorio) {
+
+		let alDip;
+		let alMeta;
+		let alOee;
+		let alPerf;
+		let alQual;
+
+		//IF Baseado na % Meta ou %OEE
+		if (Config.pctmeta == true) {
+			if (relatorio[t].pctmeta < Config.valores.vermelho) {
+				alMeta = 'vermelho'
+			} else if (relatorio[t].pctmeta < Config.valores.amarelo) {
+				alMeta = 'amarelo'
+			} else {
+				alMeta = 'verde'
+			}
+		} else {
+			if (relatorio[t].metaoee < Config.valores.vermelho) {
+				alOee = 'vermelho'
+			} else if (relatorio[t].metaoee < Config.valores.amarelo) {
+				alOee = 'amarelo'
+			} else {
+				alOee = 'verde'
+			}
+		}
+
+		if (relatorio[t].disponibilidade < Config.disponibilidade.vermelho) {
+			alDip = 'vermelho';
+		} else if (relatorio[t].disponibilidade < Config.disponibilidade.amarelo) {
+			alDip = 'amarelo';
+		} else {
+			alDip = 'verde'
+		}
+
+		if (relatorio[t].performance < Config.performance.vermelho) {
+			alPerf = 'vermelho';
+		} else if (relatorio[t].performance < Config.performance.amarelo) {
+			alPerf = 'amarelo';
+		} else {
+			alPerf = 'verde'
+		}
+
+		if (relatorio[t].performance < Config.performance.vermelho) {
+			alQual = 'vermelho';
+		} else if (relatorio[t].performance < Config.performance.amarelo) {
+			alQual = 'amarelo';
+		} else {
+			alQual = 'verde'
+		}
+		
+
 		$('#lista-relatorio').append(`
-			<tr>
-				<td>${relatorio[t].equipamento}</td>
-				<td>${Math.round(relatorio[t].producao).toFixed(1)}</td>
-				<td>${Math.round(relatorio[t].pctmeta).toFixed(1) + " %"}</td>
-				<td>${relatorio[t].descricao}</td>
-				<td>${Math.round(relatorio[t].disponibilidade).toFixed(1) + " %"}</td>
-				<td>${Math.round(relatorio[t].performance).toFixed(1) + " %"}</td>
-				<td>${Math.round(relatorio[t].qualidade).toFixed(1) + " %"}</td>
-				<td>${Math.round(relatorio[t].metaoee).toFixed(1) + " %"}</td>
-			</tr>`);
+		<tr>
+		<td>${relatorio[t].equipamento}</td>
+		<td class="text-sm-center">${Math.round(relatorio[t].producao).toFixed(1)}</td>
+		<td class="text-sm-center ${alMeta}">${Math.round(relatorio[t].pctmeta).toFixed(1) + " %"}</td>
+		<td class="text-sm-center">${relatorio[t].descricao}</td>
+		<td class="text-sm-center ${alDip}">${Math.round(relatorio[t].disponibilidade).toFixed(1) + " %"}</td>
+		<td class="text-sm-center ${alPerf}">${Math.round(relatorio[t].performance).toFixed(1) + " %"}</td>
+		<td class="text-sm-center ${alQual}">${Math.round(relatorio[t].qualidade).toFixed(1) + " %"}</td>
+		<td class="text-sm-center ${alOee}">${Math.round(relatorio[t].metaoee).toFixed(1) + " %"}</td>
+		</tr>`);
 	}
+
 }
 
  const eqp = {
