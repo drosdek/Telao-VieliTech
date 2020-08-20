0ff6bf739161e90e23c022df942f628d7c1efe0e
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-20 10:00:56 -0300

    Instrucoes e novas variaveis

--- app/config.js
+++ app/config.js
@@ -1,41 +1,70 @@
 const Config = {
+	/**
+	 * Este arquivo contém as seguintes configurações:
+	 * 
+	 * * Configuracao de alerta baseado em OEE ou % META
+	 * * Configuracao de alerta no indicador Disponibilidade
+	 * * Configuracao de alerta no indicador Performance
+	 * * Configuracao de alerta no indicador Qualidade
+	 * * Configuracao de alerta no indicador Retrabalho
+	 * * Configuracao do temporizador de transicao entre telao
+	 * 
+	 */
+
+	 /**
+	  * Aqui e definido se deseja criar o alerta baseado pela % META ou OEE
+	  * Para que funcione corretamente apenas uma das duas opcoes pode receber TRUE
+	  */
 	metaoee: true, // Marque como false caso trabalhado com PCT de Meta
-	pctmeta: false,
+	pctmeta: false,// Marque como false caso trabalhado com Meta OEE
 	valores: { // Valores para alertas
-		vermelho: 65.0, // Valores acima de
-		amarelo: 76.0, // Valores acimda de e abaixo do anterior
+		vermelho: 65.0, // Valores abaixo de
+		amarelo: 76.0, // Valores acima do anterior e abaixo de
 		verde: 100.0, // Valores abaixo de
 	},
-	pctretrabalho: true,
-	retrabalho: {
-		vermelho: 100.0, // Valores acima de
-		amarelo: 5.0, // Valores acimda de e abaixo do anterior
-		verde: 0.0, // Valores abaixo de
-	},
+
+	/** ______________________________________________ */
+	/** Area voltada para configuracao dos indicadores */
+	//Selecione True para ativar alerta na Disponibilidade
 	pctdisponibilidade: true,
 	disponibilidade: {
-		vermelho: 10.0, // Valores acima de
-		amarelo: 20.0, // Valores acimda de e abaixo do anterior
+		vermelho: 10.0, // Valores abaixo de
+		amarelo: 20.0, // Valores acima do anterior e abaixo de
 		verde: 100.0, // Valores abaixo de
 	},
+	//Selecione True para ativar alerta na Performance
 	pctperformance: true,
 	performance: {
-		vermelho: 50.0, // Valores acima de
-		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		vermelho: 50.0, // Valores abaixo de
+		amarelo: 55.0, // Valores acima do anterior e abaixo de
 		verde: 100.0, // Valores abaixo de
 	},
+	//Selecione True para ativar alerta na Performance
 	pctqualidade: true,
 	qualidade: {
-		vermelho: 50.0, // Valores acima de
-		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		vermelho: 50.0, // Valores abaixo de
+		amarelo: 55.0, // Valores acima do anterior e abaixo de
 		verde: 100.0, // Valores abaixo de
 	},
+	//Selecione True para ativar alerta na OEE
 	pctoee: true,
 	oee: {
-		vermelho: 50.0, // Valores acima de
-		amarelo: 55.0, // Valores acimda de e abaixo do anterior
+		vermelho: 50.0, // Valores abaixo de
+		amarelo: 55.0, // Valores acima do anterior e abaixo de
 		verde: 100.0, // Valores abaixo de
 	},
-	//Cada segundo = 1000
-	tempodetransicao: 15000,
+	//Selecione True para ativar alerta na Performance
+	pctretrabalho: true,
+	retrabalho: {
+		verde: 0.0, // Valores acima de
+		amarelo: 5.0, // Valores acima do anterior e
+		vermelho: 100.0, // Valores acima de
+	},
+	//Selecione o tempo de transicao entre equipamentos, cada segundo = 1000
+	tempodetransicao: 5000,
+	/**
+	 * Defina a URL para conexao
+	 */
+	conecta_url: "http://localhost:8081",
+
 }
