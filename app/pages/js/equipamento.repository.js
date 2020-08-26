9cf3c9ac383462b5b17ec8faf29e96a692ceb801
Josnei Drosdek <josneidrosdek@gmail.com>
2020-08-26 19:59:44 -0300

    Implementado leitura de objeto unico

--- app/pages/js/equipamento.repository.js
+++ app/pages/js/equipamento.repository.js
@@ -2,10 +2,11 @@ const url = Config.conecta_url+"/equipamentos"
 const equipamentos = []
 
 const request = (paramns='') => {
-	return fetch(url + paramns, {
+	return fetch(url + `${paramns}`, {
 		method: 'GET',
 	})
 		.then((res) => res.json())
+		// .then((res) => {console.log(res); return res.json()})
 		.then((response) => {
 			if (response && Array.isArray(response)) {
 				while (equipamentos.length) {
@@ -14,6 +15,11 @@ const request = (paramns='') => {
 				response.forEach(item => {
 					equipamentos.push(item);
 				});
+			} else if (response) {
+				while (equipamentos.length) {
+					equipamentos.pop()
+				}
+				equipamentos.push(response)
 			}
 		});
 }
